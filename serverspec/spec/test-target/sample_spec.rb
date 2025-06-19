f24f3b37f3fd756cb19a21ec53f7c23ea3e89da1
Michiko-Ito20 <102233651+Michiko-Ito20@users.noreply.github.com>
2025-06-19 12:51:10 +0900

    ターゲットホストのIPを使用するようにcurlコマンドを修正

--- serverspec/spec/test-target/sample_spec.rb
+++ serverspec/spec/test-target/sample_spec.rb
@@ -10,6 +10,7 @@ describe port(listen_port) do
   it { should be_listening }
 end
 
-describe command('curl http://127.0.0.1:#{listen_port}/ -o /dev/null -w "%{http_code}\n" -s') do
+# ローカルIPの代わりにターゲットホストのIPを使用
+describe command("curl http://#{ENV['TARGET_HOST']}:#{listen_port}/ -o /dev/null -w \"%{http_code}\\n\" -s") do
   its(:stdout) { should match /^200$/ }
 end
